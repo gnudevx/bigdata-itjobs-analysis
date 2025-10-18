0e8efe2104afc07156f01de366fe6af74a6aeba2
congsieunhan <hoangcongydqvcl@gmail.com>
2025-10-18 09:22:46 +0700

    Hive

--- visualization/my_streamlit_app/Home.py
+++ visualization/my_streamlit_app/Home.py
@@ -1,16 +1,15 @@
 import streamlit as st
 from dataraw import dataraw
-from page_mapreduce import page_mapreduce
-from page_web import page_web
+from page_hive import page_hive
 
 st.set_page_config(page_title="My Streamlit App", layout="wide")
 
 st.sidebar.title("Home")
-page = st.sidebar.radio("Chọn Tính Năng", ["Dữ liệu ban đầu", "MapReduce", "web"])
+page = st.sidebar.radio("Chọn Tính Năng", ["Dữ liệu ban đầu", "Hive Analysis", "web"])
 
 if page == "Dữ liệu ban đầu":
     dataraw()
-elif page == "MapReduce":
-    page_mapreduce()
+elif page == "Hive Analysis":
+    page_hive()
 elif page == "web":
     page_web()
